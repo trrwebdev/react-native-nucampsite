90e56132ba5a274a374acf87407f51c18105cec2
Terri Read <trrwebdev@gmail.com>
2022-11-11 23:07:54 -0600

    Custom Drawer Header

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,15 +1,18 @@
-import { Platform, View } from 'react-native';
+import { Image, Platform, StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
 import DirectoryScreen from './DirectoryScreen';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {
+    createDrawerNavigator,
+    DrawerContentScrollView,
+    DrawerItemList
+} from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
 import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
-import { StyleSheet } from 'react-native';
 import { Icon } from 'react-native-elements';
-
+import logo from '../assets/images/logo.png';
 
 const Drawer = createDrawerNavigator();
 
@@ -119,6 +122,20 @@ const DirectoryNavigator = () => {
     );
 };
 
+const CustomDrawerContent = (props) => (
+    <DrawerContentScrollView {...props}>
+        <View style={styles.drawerHeader}>
+            <View style={{ flex: 1 }}>
+                <Image source={logo} style={styles.drawerImage} />
+            </View>
+            <View style={{ flex: 2 }}>
+                <Text style={styles.drawerHeaderText}>NuCamp</Text>
+            </View>
+        </View>
+        <DrawerItemList {...props} labelStyle={{ fontWeight: 'bold' }} />
+    </DrawerContentScrollView>
+);
+
 const Main = () => {
     return (
         <View
@@ -130,6 +147,7 @@ const Main = () => {
         >
             <Drawer.Navigator
                 initialRouteName='Home'
+                drawerContent={CustomDrawerContent}
                 drawerStyle={{ backgroundColor: '#CEC8FF' }}
             >
                 <Drawer.Screen
@@ -142,11 +160,11 @@ const Main = () => {
                                 name='home'
                                 type='font-awesome'
                                 size={24}
+                                iconStyle={{ width: 24 }}
                                 color={color}
                             />
                         )
                     }}
-
                 />
                 <Drawer.Screen
                     name='Directory'
@@ -158,6 +176,7 @@ const Main = () => {
                                 name='list'
                                 type='font-awesome'
                                 size={24}
+                                iconStyle={{ width: 24 }}
                                 color={color}
                             />
                         )
@@ -173,6 +192,7 @@ const Main = () => {
                                 name='info-circle'
                                 type='font-awesome'
                                 size={24}
+                                iconStyle={{ width: 24 }}
                                 color={color}
                             />
                         )
@@ -188,6 +208,7 @@ const Main = () => {
                                 name='address-card'
                                 type='font-awesome'
                                 size={24}
+                                iconStyle={{ width: 24 }}
                                 color={color}
                             />
                         )
@@ -199,6 +220,24 @@ const Main = () => {
 };
 
 const styles = StyleSheet.create({
+    drawerHeader: {
+        backgroundColor: '#5637DD',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        height: 60,
+        width: 60
+    },
     stackIcon: {
         marginLeft: 10,
         color: '#fff',
